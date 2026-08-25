c4e1ea5fb4169d3dea59407daaebbe2b1c9f23e4
urud289-hash <urud289@gmail.com>
2026-08-25 14:42:29 +0300

    Update GHOST.py

--- GHOST.py
+++ GHOST.py
@@ -376,7 +376,7 @@ st.title(
 # 09. KENAR ÇUBUĞU (OMEGA YÖNETİM MENÜSÜ)
 # ==========================================
 st.sidebar.markdown(
-    <h3 style="color: #38bdf8; margin-top:0;">&#128225; Canlı Uydu & Harita Radarı</h3>
+   <h3 style="color: #38bdf8; margin-top:0;">Canlı Uydu & Harita Radarı</h3>
     " Menü</h3>",
     unsafe_allow_html=True,
 )
